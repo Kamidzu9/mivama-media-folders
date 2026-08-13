4df2ea03bdd657142e3dc0eda93d95aa2c04f570
Mykhailo Solovei <42532634+Kamidzu9@users.noreply.github.com>
2026-08-13 10:33:02 +0200

    build: enforce plugin version consistency

---
+++ bin/check-version.php
@@ -0,0 +1,36 @@
+<?php
+
+$root = dirname(__DIR__);
+$plugin = file_get_contents($root . '/mivama-media-folders.php');
+$class = file_get_contents($root . '/includes/class-mivama-media-folders.php');
+$readme = file_get_contents($root . '/readme.txt');
+
+if (! preg_match('/^ \* Version:\s*([^\s]+)$/m', $plugin, $plugin_match)) {
+    fwrite(STDERR, "Could not read plugin header version.\n");
+    exit(1);
+}
+
+if (! preg_match("/const VERSION = '([^']+)'/", $class, $class_match)) {
+    fwrite(STDERR, "Could not read class VERSION constant.\n");
+    exit(1);
+}
+
+if (! preg_match('/^Stable tag:\s*([^\s]+)$/m', $readme, $readme_match)) {
+    fwrite(STDERR, "Could not read readme stable tag.\n");
+    exit(1);
+}
+
+$versions = array(
+    'plugin header' => $plugin_match[1],
+    'class constant' => $class_match[1],
+    'stable tag' => $readme_match[1],
+);
+
+if (count(array_unique(array_values($versions))) !== 1) {
+    foreach ($versions as $source => $version) {
+        fwrite(STDERR, sprintf("%s: %s\n", $source, $version));
+    }
+    exit(1);
+}
+
+echo 'Version OK: ' . $plugin_match[1] . PHP_EOL;
